c1df47dfc0b911d720a0930077efa43e7b1c27ab
Melania P Chagas <melaniapchagas@gmail.com>
2022-12-07 18:08:44 -0300

    Criando migration e model para a tabela 'matches'

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,52 @@
+import { BOOLEAN, Model, INTEGER } from 'sequelize';
+import db from '.';
+
+class Matches extends Model {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamsGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+
+  awayTeam: {
+    allowNull: false,
+    type: INTEGER,
+  },
+
+  awayTeamsGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+
+  inProgress: {
+    allowNull: false,
+    type: BOOLEAN,
+  },
+
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+export default Matches;
