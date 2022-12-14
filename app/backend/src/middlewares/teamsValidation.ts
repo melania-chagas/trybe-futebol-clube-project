4a511254d0b09049e1ff59c8c7eddcd188421728
Melania P Chagas <melaniapchagas@gmail.com>
2022-12-13 21:29:38 -0300

    Implementando verificação no middleware 'teamsValidation'

--- app/backend/src/middlewares/teamsValidation.ts
+++ app/backend/src/middlewares/teamsValidation.ts
@@ -1,12 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
+import Team from '../database/models/Team';
 
-const teamsValidation = (req: Request, res: Response, next: NextFunction) => {
+const teamsValidation = async (req: Request, res: Response, next: NextFunction) => {
   const { homeTeam, awayTeam } = req.body;
 
   if (homeTeam === awayTeam) {
     return res.status(422).json({
       message: 'It is not possible to create a match with two equal teams',
     });
+  }
+
+  const checkIfExistsHomeTeam = await Team.findByPk(homeTeam);
+  const checkIfExistsAwayTeam = await Team.findByPk(awayTeam);
+
+  if (!checkIfExistsHomeTeam || !checkIfExistsAwayTeam) {
+    return res.status(404).json({ message: 'There is no team with such id!' });
   }
 
   next();
